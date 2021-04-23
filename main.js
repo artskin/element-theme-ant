e9de22c29112fe7ae77bd0b220bb3d3aac6e829d
artskin <artskin@163.com>
2021-04-23 23:50:52 +0800

    add element-ui theme

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.scss'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!-less</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation2</a>
+  <button class="el-button">按钮</button>
+  <div class="el-tag el-icon-dish">标签</div>
+  <input class="el-input" />
+`
